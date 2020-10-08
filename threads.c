798549c8e1a1a6ef026245e4d5fe2c2d1998e55f
Mrinal Ghosh <ghoshm@bu.edu>
2020-10-08 13:32:41 -0400

    prototypes added

--- threads.c
+++ threads.c
@@ -1,12 +1,15 @@
 #include "threads.h"
 
-// #include <pthreads.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/* FUNCTION HEADERS FOR PTHREADS
-int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-                   void *(*start_routine)(void *), void *arg);
+#include "ec440threads.h"
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
+
 void pthread_exit(void *value_ptr);
+
 pthread_t pthread_self(void);
-*/
+
+void schedule(void);
--- threads.h
+++ threads.h
@@ -3,23 +3,38 @@
 
 #include <pthread.h>
 #include <setjmp.h>
+#include <signal.h>
 
-#define QUOTA_MS 50 #milliseconds
-#define QUOTA_US 50000 #microseconds
-#define STACK_SIZE 32767 #bytes
+#define QUOTA 50000       // microseconds
+#define STACK_SIZE 32767  // bytes
+#define MAX_THREADS 128
+/* Register name definitions from libc
+JB_RBX 0
+JB_RBP 1
+JB_R12 2
+JB_R13 3
+JB_R14 4
+JB_R15 5
+JB_RSP 6
+JB_PC 7
+*/
 
 typedef enum state {
     RUNNING,
     READY,
     EXITED,
-    // BLOCKED,
 } state_t;
 
 typedef struct TCB {
     pthread_t tid;
-    jmp_buf buf;
-    void * stack_p;
+    jmp_buf buf;  // 64b[8]
+    void *stack;
     state_t state;
 } TCB_t;
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
+void pthread_exit(void *value_ptr);
+pthread_t pthread_self(void);
+void schedule(void);
 
 #endif
