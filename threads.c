6996ba6365cfcfd33815a0701a415dd7453d47ee
Mrinal Ghosh <ghoshm@bu.edu>
2020-10-11 23:42:44 -0400

    cleanish

--- threads.c
+++ threads.c
@@ -1,13 +1,11 @@
 #include "threads.h"
 
-// #include <pthread.h>
-// #include <setjmp.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // memset
-#include <unistd.h>  // ualarm
+#include <string.h>
+#include <unistd.h>
 
 #include "ec440threads.h"
 
@@ -15,43 +13,15 @@ int thread_count = 0;
 TCB *head = NULL;
 TCB *current = NULL;
 
-// void inspect_jmpbuf(jmp_buf buf) {
-//     printf(
-//         "RBX: 0x%08lx\nRBP: 0x%08lx\nR12: 0x%08lx\nR13: 0x%08lx\nR14: 0x%08lx\nR15: 0x%08lx\nSP:  0x%08lx\nPC:  0x%08lx\n",
-//         buf[0].__jmpbuf[JB_RBX], buf[0].__jmpbuf[JB_RBP],
-//         buf[0].__jmpbuf[JB_R12], buf[0].__jmpbuf[JB_R13],
-//         buf[0].__jmpbuf[JB_R14], buf[0].__jmpbuf[JB_R15],
-//         buf[0].__jmpbuf[JB_RSP], buf[0].__jmpbuf[JB_PC]);
-
-//     printf("RSPd:0x%08lx\nPCd: 0x%08lx\n",
-//            ptr_demangle(buf[0].__jmpbuf[JB_RSP]),
-//            ptr_demangle(buf[0].__jmpbuf[JB_PC]));
-// }
-
-// void inspect_thread(TCB *thread) {
-//     inspect_jmpbuf(thread->buf);
-//     printf("stack:%p\nstate:%d\n", thread->stack, thread->state);
-//     printf("next:%p\nlast:%p\n", thread->next, thread->prev);
-// }
-
 void scheduler(int signum) {
-    // printf("-------8<-------------[ cut here ]-------------\n");
-    // printf("Scheduling: thread %d ===> thread %d\n", (unsigned)current->id, (unsigned)current->next->id);
-    // inspect_thread(current);
-
     if (!setjmp(current->buf)) {  // save current buffer
-        // current->state = READY;   // stop running current thread
-
-        // IMPLEMENTATION WITH KEEPING EXITED THREADS IN LINKED LIST
         do {
             if (current->state == RUNNING)
                 current->state = READY;
-            current = current->next;  // go to next thread
-            // printf("GOING TO THREAD %d with STATE:%d\n", (unsigned)current->id, current->state);
+            current = current->next;
         } while (current->state == EXITED);
 
-        // current = current->next;   // at this point we have lost track of the current thread - WITHOUT explicitly freeing it :(
-        current->state = RUNNING;  // set state as running
+        current->state = RUNNING;  // mark next thread - running
         longjmp(current->buf, 1);  // longjmp to next thread
     }
 }
@@ -59,13 +29,11 @@ void scheduler(int signum) {
 static void thread_init(void) {
     TCB *mt = (TCB *)calloc(1, sizeof(TCB));
 
-    // set head and current to be main thread
-    head = mt;
-    current = mt;
-
-    // initialize TCB
+    head = mt;     // used to keep track of where to insert
+    current = mt;  // used in scheduler
     mt->next = mt;
-    mt->prev = mt;     // circular list with single element
+    mt->prev = mt;  // circular singleton
+
     mt->stack = NULL;  // main has a REAL stack
     mt->state = RUNNING;
     mt->id = (pthread_t)thread_count;
@@ -74,10 +42,8 @@ static void thread_init(void) {
 
     ++thread_count;
 
-    // signal handling
     struct sigaction act;
-    memset(&act, '\0', sizeof(act));  // zero struct
-
+    memset(&act, '\0', sizeof(act));  // calloc instead of static var?
     act.sa_handler = scheduler;
     act.sa_flags = SA_NODEFER;  // can even recieve alarm signal within handler
 
@@ -87,71 +53,48 @@ static void thread_init(void) {
     }
 
     ualarm(QUOTA, QUOTA);
-
-    // inspect_thread(mt);
 }
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
-    // printf("Inside pthread_create()\nThread count: %d\n", thread_count);
-
-    if (thread_count == 0) {  // initialize single main thread
+    if (!thread_count) {
         thread_init();
     }
 
     if (thread_count >= MAXTHREADS) {
-        perror("maximum threads created!");
+        perror("max threads reached");
         exit(1);
     }
 
-    // create thread
-    TCB *new_thread = (TCB *)calloc(1, sizeof(TCB));
+    TCB *new_thread = (TCB *)calloc(1, sizeof(TCB));  // calloc not malloc - don't have to memset null byte
 
-    // add to end of circular linked list
-    new_thread->prev = head->prev;
+    new_thread->prev = head->prev;  // add to end of circular linked list
     head->prev->next = new_thread;
     head->prev = new_thread;
     new_thread->next = head;
 
-    // save thread id
     new_thread->id = (pthread_t)thread_count;
-    *thread = new_thread->id;
+    *thread = new_thread->id;  // probably not needed - process calls pthread_self anyway
 
-    // allocate memory to stack
-    new_thread->stack = calloc(1, STACKSIZE);
+    new_thread->stack = calloc(1, STACKSIZE);  // allocate thread stack - THIS IS NEVER FREED - NOT ERRORING BUT FIX :/
 
-    // populate tcb
     setjmp(new_thread->buf);
     new_thread->state = READY;
-
-    new_thread->stack = new_thread->stack + STACKSIZE - 8;  // 8 from the top - stack starts from high address - camden OH
+    new_thread->stack = new_thread->stack + STACKSIZE - 8;  // 8 from the top - stack starts from high addresses - Camden OH
     *(unsigned long *)new_thread->stack = (unsigned long)pthread_exit;
 
-    // mangle RIP/PC and RSP
     new_thread->buf[0].__jmpbuf[JB_R12] = (unsigned long)start_routine;
-    new_thread->buf[0].__jmpbuf[JB_R13] = (unsigned long)arg;                     // ->rdi
-    new_thread->buf[0].__jmpbuf[JB_PC] = ptr_mangle((unsigned long)start_thunk);  // ->pc - from camden OH
+    new_thread->buf[0].__jmpbuf[JB_R13] = (unsigned long)arg;
+    new_thread->buf[0].__jmpbuf[JB_PC] = ptr_mangle((unsigned long)start_thunk);
     new_thread->buf[0].__jmpbuf[JB_RSP] = ptr_mangle((unsigned long)new_thread->stack);
 
     ++thread_count;
-
     return 0;
 }
 
 void pthread_exit(void *value_ptr) {
     current->state = EXITED;
-
-    // pop from circ linked list
-    // current->prev->next = current->next;
-    // current->next->prev = current->prev;
-
-    // TODO: clean up by freeing exited threads - maybe maintain a secondary linked list of things to delete when thread count hits 1? main ends?
-    // free(current->stack); // "free(): invalid pointer"
-
-    --thread_count;  // since tid is only unique to threads which have not exited - can reuse
-
-    // exit(0) once prev thread has exited - IF IT DOESN'T PERROR or SEGFAULT earlier, the exit code is zero automatically
-
-    scheduler(SIGALRM);       // return to scheduler early
+    --thread_count;           // since tid is only unique to threads which have not exited - can reuse
+    scheduler(SIGALRM);       // return to scheduler early - unfair quota for next thread?
     __builtin_unreachable();  // GNU compiler built in - never return to this function if we hit this
 }
 
